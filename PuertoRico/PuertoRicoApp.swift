69b32b64f7016c5ce1cbcf84da87fd4151f1ba40
Christoph Freier <christoph@freier.fr>
2026-03-30 10:33:00 +0200

    Initial Commit

---
+++ PuertoRico/PuertoRicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuertoRicoApp.swift
+//  PuertoRico
+//
+//  Created by Christoph Freier on 30.03.26.
+//
+
+import SwiftUI
+
+@main
+struct PuertoRicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
